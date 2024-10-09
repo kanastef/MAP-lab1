b94768fbc8bc71354eacc82b060647402793b77c
ana kogalniceanu <anastef10@yahoo.com>
2024-10-09 12:50:22 +0300

    final commit grades

--- src/Grades.java
+++ src/Grades.java
@@ -14,8 +14,8 @@ public class Grades {
 
     public static double average(int[] grades) {
         int sum = 0;
-        for (int i = 0; i < grades.length; i++) {
-            sum += grades[i];
+        for (int grade : grades) {
+            sum += grade;
         }
         return (double) sum / grades.length;
     }
@@ -23,16 +23,26 @@ public class Grades {
     public static int[] round(int[] grades) {
         int[] roundedGrades = new int[grades.length];
         int count = 0;
-        for (int i = 0; i < grades.length; i++) {
-            if (grades[i] >= 38 && 5 - (grades[i] % 5) < 3){
-                roundedGrades[count] = grades[i] + (5 - grades[i] % 5);
+        for (int grade : grades) {
+            if (grade >= 38 && 5 - (grade % 5) < 3) {
+                roundedGrades[count] = grade + (5 - grade % 5);
                 count++;
-            }
-            else {
-                roundedGrades[count] = grades[i];
+            } else {
+                roundedGrades[count] = grade;
                 count++;
             }
         }
         return roundedGrades;
+    }
+
+    public static int maxRounded(int[] grades) {
+        grades = round(grades);
+        int max = grades[0];
+        for (int grade : grades) {
+            if (grade > max) {
+                max = grade;
+            }
+        }
+        return max;
     }
 }
